b28f5a99c48c13dec6262d6c45dba572ee369cc5
arisa <arisa8533@gmail.com>
2026-03-02 02:56:53 +0700

    [BE] update permission gaurd on user controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -15,16 +15,22 @@ import {
 } from './interface/user.interface';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { PermissionsGuard } from 'src/auth/guards/permissions.guard';
+import { RequirePermissions } from 'src/auth/decorators/permissions.decorator';
+import { PermissionEnum } from 'src/enum/permission';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @RequirePermissions(PermissionEnum.VIEW_USER)
   @Get()
   async getAllUser(): Promise<Array<GetUserResponse>> {
     return await this.usersService.getAllUser();
   }
 
+  @RequirePermissions(PermissionEnum.VIEW_USER)
   @Get(':id')
   async getUserDetails(
     @Param('id') id: string,
@@ -32,11 +38,13 @@ export class UsersController {
     return await this.usersService.getUserDetails(id);
   }
 
+  @RequirePermissions(PermissionEnum.EDIT_USER_INFO)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateData: UpdateUserDto) {
     return this.usersService.updateUser(id, updateData);
   }
 
+  @RequirePermissions(PermissionEnum.CAN_DELETE)
   @Delete(':id')
   async removeUser(@Param('id', ParseUUIDPipe) userId: string) {
     return this.usersService.softDeleteUser(userId);

7f6e87a5eff28a3b309df359b23c8971302b0e82
arisa <arisa8533@gmail.com>
2026-03-02 11:35:20 +0700

    [BE] Add prmission guard for dashboard controller

--- backend/src/dashboard/dashboard.controller.ts
+++ backend/src/dashboard/dashboard.controller.ts
@@ -2,11 +2,16 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { DashboardService } from './dashboard.service';
 import { DashboardResponse } from './interface/dashboard.interface';
+import { PermissionsGuard } from 'src/auth/guards/permissions.guard';
+import { RequirePermissions } from 'src/auth/decorators/permissions.decorator';
+import { PermissionEnum } from 'src/enum/permission';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
+
+  @RequirePermissions(PermissionEnum.VIEW_DASHBOARD)
   @Get()
   async getDashboard(): Promise<DashboardResponse> {
     return await this.dashboardService.getDashboard();
